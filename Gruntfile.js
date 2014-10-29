fe52b3112dec73af69e6b7d4aeec96d762afed9d
Mohamed-amin <mohamed.amin@espace.com.eg>
2014-10-29 18:13:08 +0200

    concat Styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,51 @@ module.exports = function(grunt) {
     // 1. All configuration goes here 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
-       concat: {   
-            dist: {
-                src: [
-                    'js/jquery-2.1.1.min.js', // All JS in the libs folder
-                    'js/chosen.jquery.min.js', // All JS in the libs folder
-                    'js/bootstrap.min.js', // All JS in the libs folder
-                    'js/*.js', // All JS in the libs folder                    
-                    'js/*.json', // All JS in the libs folder
-                ],
-                dest: 'js/build/production.js',
-            }
+    concat_css: {
+        options: {
+          // Task-specific options go here.
         },
+        all: {
+          src: [
+                "/**/*.css",
+                "/**/*.css",
+          ],
+          dest: "css/build/styles.css"
+        },
+    },
+     concat: {
+        css: {
+           src: [
+                 'css/bootstrap.min.css',
+                 'css/bootstrap.min-flat.css',
+                 'css/chosen.min.css'     
+                ],
+            dest: 'css/build/styles.css'
+        },
+        js : {
+            src : [
+               'js/jquery-2.1.1.min.js', // All JS in the libs folder
+                  'js/chosen.jquery.min.js', // All JS in the libs folder
+                  'js/bootstrap.min.js', // All JS in the libs folder
+                  'js/*.js', // All JS in the libs folder                    
+                  'js/*.json', // All JS in the libs folder
+            ],
+            dest: 'js/build/production.js',
+        }
+    },
+    concat: { 
+
+          dist: {
+              src: [
+                  'js/jquery-2.1.1.min.js', // All JS in the libs folder
+                  'js/chosen.jquery.min.js', // All JS in the libs folder
+                  'js/bootstrap.min.js', // All JS in the libs folder
+                  'js/*.js', // All JS in the libs folder                    
+                  'js/*.json', // All JS in the libs folder
+              ],
+              dest: 'js/build/production.js',
+          }
+    },
     uglify: {
         build: {
             src: 'js/build/production.js',
@@ -39,7 +71,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
+    grunt.loadNpmTasks('grunt-concat-css');
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['concat','uglify']);
 
